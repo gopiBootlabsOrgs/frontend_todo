1468533a7f6ff4f1c8bb31d9f865a8dcc3531e3f
gopinathbootlabs <gopinath.kannan@bootlabstech.com>
2023-02-21 17:34:35 +0530

    frontend

--- react-table/src/index.js
+++ react-table/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Home from './route/Home'
-import C1 from './route/C1';
-import C2 from './route/C2';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import C1 from './route/C1'
+import Home from './route/Home'
+import C2 from './route/C2'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={< C1 />}></Route>
+    <Route path='/view' element={< C2 />} ></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
